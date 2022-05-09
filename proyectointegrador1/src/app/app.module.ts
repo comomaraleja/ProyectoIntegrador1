422fac604bade1faec354af37725b74d3015a2db
Alejandra Correa <comomaraleja@gmail.com>
2022-05-09 02:08:02 -0300

    Modificación header (logos y efecto texto)

--- proyectointegrador1/src/app/app.module.ts
+++ proyectointegrador1/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
