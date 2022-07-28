c438610af98580dece7d2b5053328996b01c5745
Alejandra Correa <comomaraleja@gmail.com>
2022-07-28 18:37:22 -0300

    Experiencia terminada

--- proyectointegrador1/FrontEnd/src/app/app-routing.module.ts
+++ proyectointegrador1/FrontEnd/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
@@ -7,7 +8,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'login',component:LoginComponent},
-  {path:'nuevaexp', component: NewExperienciaComponent}
+  {path:'nuevaexp', component: NewExperienciaComponent},
+  {path:'editexp/:id', component: EditExperienciaComponent}
 ];
 
 @NgModule({
--- proyectointegrador1/FrontEnd/src/app/components/experiencia/edit-experiencia.component.ts
+++ proyectointegrador1/FrontEnd/src/app/components/experiencia/edit-experiencia.component.ts
@@ -1,4 +1,7 @@
+import { SExperienciaService } from './../../services/sExperiencia.service';
+import { Experiencia } from './../../model/experiencia';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-experiencia',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-experiencia.component.css']
 })
 export class EditExperienciaComponent implements OnInit {
-
-  constructor() { }
+ expLab: Experiencia = null;
+ 
+  constructor(private sExperienciaService: SExperienciaService, private activatedRouter: ActivatedRoute,  private router: Router){ }
 
   ngOnInit(): void {
+    //captura el id de la expereinecia que queremos modificar
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.sExperienciaService.detail(id).subscribe(
+      data =>{
+        this.expLab = data;
+      }, err =>{
+        alert("Error al modificar experiencia");
+        this.router.navigate(['']);
+      }
+    )
+  }
+
+  onUpdate(): void{
+    //busca por id el metodo que tiene que editar
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.sExperienciaService.update(id, this.expLab).subscribe(
+      data => {
+        this.router.navigate(['']);
+      }, err =>{
+         alert("Error al modificar experiencia");
+         this.router.navigate(['']);
+      }
+    )
   }
 
 }
+
